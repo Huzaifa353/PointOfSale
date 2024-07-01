7ab9e2d44e59867805a6602c8f01ef88a1341ab5
Huzaifa Shamoon <huzaifa.shamoon@quantum-computing-services.com>
2024-07-01 19:41:42 +0500

    Minor Update

--- setup.bat
+++ setup.bat
@@ -4,32 +4,25 @@ echo Starting Laravel setup...
 :: Set the current directory to the script's directory
 cd /d %~dp0
 
-:: Check if PHP is installed
-echo Checking if PHP is installed...
-where php >nul 2>nul
-if errorlevel 1 (
-    echo PHP is not installed. Please download and install PHP manually from https://www.php.net/downloads.php
-    pause
-    exit /b 1
-)
-
 :: Check if Composer is installed
 echo Checking if Composer is installed...
 where composer >nul 2>nul
 if errorlevel 1 (
-    echo Composer is not installed. Please install it and try again.
+    echo Composer is not installed. Please install it from the SetupTools folder or get it from https://getcomposer.org/download/. 
     pause
     exit /b 1
 )
+echo
 
 :: Check if MySQL is installed
 echo Checking if MySQL is installed...
 where mysql >nul 2>nul
 if errorlevel 1 (
-    echo MySQL is not installed. Please download MySQL from https://dev.mysql.com/downloads/ and install it manually.
+    echo MySQL is not installed. Please install it from the SetupTools folder or get it from https://dev.mysql.com/downloads/.
     pause
     exit /b 1
 )
+echo
 
 :: Check if .env file exists, if not copy from .env.example
 IF NOT EXIST .env (
@@ -38,6 +31,7 @@ IF NOT EXIST .env (
     echo .env file Created Successfully
     echo Now please configure .env file and run this setup again
 )
+echo
 
 set /p db_password=Enter MySQL database password: 
 
@@ -52,27 +46,32 @@ IF NOT EXIST vendor (
 ) ELSE (
     echo Dependencies are already installed, skipping installation.
 )
+echo
 
 :: Generate application key
 echo Generating application key...
 php artisan key:generate
+echo
 
 :: Migrate the database
 echo Migrating the database...
 php artisan migrate
+echo
 
 :: Seed the database
 echo Seeding Database
 php artisan db:seed --class=GeneralSettingsSeeder
+echo
 
 :: Clear cache
 echo Clearing cache...
 php artisan config:cache
+echo
+
+echo Laravel setup completed.
 
 :: Start Laravel development server
 echo Starting Laravel development server...
 start php -S localhost:8000 -t public
 
-
-echo Laravel setup completed.
 pause
